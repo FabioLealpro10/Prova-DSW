74b2540f29d6c58f92ced6034a85c69fe629bff7
Fabio Leal <fl2646730@gmail.com>
2026-04-18 12:19:26 -0400

    final
    This script handles event form submissions, stores event data, and displays a list of events. It also includes a search feature for filtering events by title.

---
+++ JS/main.js
@@ -0,0 +1,74 @@
+let eventos = []
+
+const formulario = document.getElementById('FormEvento')
+
+formulario.addEventListener("submit", function (event) {
+
+    event.preventDefault() 
+
+    let titulo = document.getElementById('titulo').value
+    let categoria = document.getElementById('categoria').value
+    let vagas = document.getElementById('vagas').value
+    let data = document.getElementById('data').value
+    let local = document.getElementById('local').value
+    let descricao = document.getElementById('descricao').value
+
+    let dados = {
+        titulo,
+        categoria,
+        vagas,
+        data,
+        local,
+        descricao
+    }
+
+    eventos.push(dados)
+
+    listar_eventos(eventos)
+
+    formulario.reset() // limpa formulário
+})
+
+function listar_eventos(dados_eventos) {
+
+    const listaEventos = document.getElementById('listaEventos')
+
+    let dados = ''
+
+    dados_eventos.forEach(evento => {
+
+        dados += `
+            <div class="card">
+                <h3>${evento.titulo}</h3>
+
+                <p><strong>Categoria:</strong> ${evento.categoria}</p>
+
+                <p><strong>Data:</strong> ${evento.data}</p>
+
+                <p><strong>Local:</strong> ${evento.local}</p>
+
+                <p><strong>Vagas:</strong> ${evento.vagas}</p>
+
+                <p><strong>Descrição:</strong> ${evento.descricao}</p>
+            </div>
+        `
+    })
+
+    listaEventos.innerHTML = dados
+}
+
+
+const pesquisar_titulo = document.getElementById('inputPesquisa')
+pesquisar_titulo.addEventListener("input", function (){
+     let titulo = document.getElementById('titulo').value
+
+     let dados_pesquisados = []
+     eventos.forEach(dados => {
+        if (dados.titulo.includes(titulo)){
+            dados_pesquisados.push(dados)
+        }
+        henderizar_eventos(dados_pesquisados)
+
+   
+    });
+})
